2df597547dd9d977f15853e6db84ae12737e6e53
Andrey <and736v@mail.ru>
2019-04-24 05:43:26 +0400

    lesson-5

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -4,7 +4,9 @@ namespace app\controllers;
 
 use Yii;
 use app\models\User;
+use app\models\Task;
 use yii\data\ActiveDataProvider;
+use yii\helpers\VarDumper;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -130,12 +132,58 @@ class UserController extends Controller
      */
     public function actionTest()
     {
-        //используя ActiveRecord (метод класса User) создадим запись в таблице user
+        // создадим запись в таблице user
         $user = new User();
-        $user->username = 'George';
-        $user->password_hash = 'password-4';
-        $user->creator_id = '4';
+        $user->username = 'Nikolay';
+        $user->password_hash = 'password-9';
+        $user->creator_id = '9';
         $user->created_at = time();
-        $user->save();
+        _log($user->save());
+
+
+        // создадим три связаные (с записью в user) запиcи в task, используя метод link()
+        $task_1 = new Task();
+        $task_1->title = 'new_task_1';
+        $task_1->description = 'work_1';
+        $task_1->created_at = time();
+        $task_1->link(Task::RELATION_CREATOR, $user);
+        $task_2 = new Task();
+        $task_2->title = 'new_task_2';
+        $task_2->description = 'work_2';
+        $task_2->created_at = time();
+        $task_2->link(Task::RELATION_CREATOR, $user);
+        $task_3 = new Task();
+        $task_3->title = 'new_task_3';
+        $task_3->description = 'work_3';
+        $task_3->created_at = time();
+        $task_3->link(Task::RELATION_CREATOR, $user);
+
+
+        // прочитаем из базы все записи из User, применив жадную подгрузку связанных данных из Task,
+        // с запросами без JOIN.
+        $models = Task::find()->with(Task::RELATION_CREATOR)->asArray()->all();
+        _log($models);
+
+
+        // прочитаем из базы все записи из User, применив жадную подгрузку связанных данных из Task,
+        // с запросом содержащим JOIN.
+        $models = Task::find()->joinWith(Task::RELATION_CREATOR)->asArray()->all();
+        _log($models);
+
+        // проверить - добавить с помощью релейшена getAccessedUsers связь между записями в Task и User (метод link())
+        // и получить из модели задачи список пользователей которым она доступна.
+        $user = User::findOne(4);
+        $task = Task::findOne(3);
+        $task->link(Task::RELATION_ACCESSED_USERS, $user);
+        _log($task->getAccessedUsers()->all());
+
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => User::find(),
+        ]);
+
+        return $this->render('test', [
+            'dataProvider' => $dataProvider,
+        ]);
     }
 }
