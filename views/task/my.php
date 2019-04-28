db8a987c7ba4ed17e4298bcaa400965120b18b77
Andrey <and736v@mail.ru>
2019-04-28 14:39:56 +0400

    lesson-7

---
+++ views/task/my.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Tasks';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="task-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Task', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php Pjax::begin(); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            'title',
+            'description:ntext',
+            'created_at:datetime',
+            'updated_at:datetime',
+
+            [
+                'class' => 'yii\grid\ActionColumn',
+                'template' => [],
+                'buttons' => []
+            ],
+        ],
+    ]); ?>
+
+    <?php Pjax::end(); ?>
+
+</div>
